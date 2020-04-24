65435ce836f77a9bc4df5491a16a6c8f00befbcc
bvalean <bogdan.valean@codemart.ro>
2020-04-24 15:35:10 +0300

    TVB-2523 Mac build

---
+++ tvb_build/create_pip_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip3 install nomkl numba scipy numpy networkx scikit-learn cython numexpr psutil
+pip3 install pytest pytest-cov pytest-benchmark pytest-mock matplotlib-base
+pip3 install psycopg2 pytables scikit-image==0.14.2 simplejson cherrypy docutils werkzeug==0.16.1
+pip3 install conda-forge jupyterlab flask gevent
+pip3 install h5py>=2.10 formencode cfflib jinja2 nibabel sqlalchemy==1.1.14 sqlalchemy-migrate==0.11.0 allensdk
+pip3 install tvb-gdist typing BeautifulSoup4 subprocess32 flask-restplus python-keycloak mako
+pip3 install py2app docutils apscheduler pyobjc
+pip3 install --upgrade setuptools
+pip3 install --upgrade distribute
